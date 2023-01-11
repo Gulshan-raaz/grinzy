43aa3bcebbe50d2e9b5222965d2457a108163ae6
Gulshan Raj <gulshanraj7399@gmail.com>
2023-01-11 18:21:58 +0530

    updated

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
   {
     business_Name: { type: String, required: true},
-    business_Email: { type: String},
+   
     business_Number: { type: String, required: true, unique: true },
     business_Address: { type: Object, required: true},
     business_Password: { type: String, required: true },
